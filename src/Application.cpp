f175965c647ab76ed423fc0f6a2a2307093538ff
sweettirexkiller <piotrjankiewicz02@gmail.com>
2023-12-28 19:07:23 +0100

    vertex attributes

--- src/Application.cpp
+++ src/Application.cpp
@@ -33,6 +33,15 @@ int main(void)
     /* print opengl version and which driver is used */
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    unsigned int buffer;
+    float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -47,6 +56,11 @@ int main(void)
         //glVertex2f( 0.0f,  0.5f);
         //glVertex2f( 0.5f, -0.5f);
         //glEnd();
+
+        // this is modern opengl code
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
 
 
         /* Swap front and back buffers */
